59c5e70863a64b5e6549fa1cd782e8770197479f
lwmfjc <lwmfjc@gmail.com>
2022-05-09 17:09:03 +0800

    flowable-测试项目

---
+++ src/main/java/com/ly/MyApplication.java
@@ -0,0 +1,40 @@
+package com.ly;
+
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(proxyBeanMethods = false)
+public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner init(final RepositoryService repositoryService,
+                                  final RuntimeService runtimeService,
+                                  final TaskService taskService) {
+
+        //该bean在项目服务启动的时候就去加载一些数据
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... strings) throws Exception {
+                //有几个流程定义
+                System.out.println("Number of process definitions : "
+                        + repositoryService.createProcessDefinitionQuery().count());
+                /*
+                //有多少个任务
+                System.out.println("Number of tasks : " + taskService.createTaskQuery().count());
+                runtimeService.startProcessInstanceByKey("oneTaskProcess");
+                //开启流程后有多少个任务（+1）
+                System.out.println("Number of tasks after process start: "
+                        + taskService.createTaskQuery().count());*/
+                //myService.createDemoUsers();
+            }
+        };
+    }
+}
---
+++ src/main/java/com/ly/controller/MyController.java
@@ -0,0 +1,8 @@
+package com.ly.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("flowable")
+public class MyController {
+
+}
